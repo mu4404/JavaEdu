b6af964e9a3bfbd8d93b63532957634eec31752e
mu4404 <ckdals4693@gmail.com>
2020-08-04 16:36:31 +0900

    첫업로드

---
+++ Ajax-jsp/WebContent/ajax/ajaxCommunication.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+  $("#ajaxConGetButton").click(function () {
+    AjaxConGet();
+    console.log("1111");
+  });
+
+  $("#ajaxConPostButton").click(function () {
+    AjaxConPost();
+  });
+});
+
+function AjaxConGet() {
+  var url = "http://localhost:8081/Ajax-jsp/ajaxCon";
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "html",
+    data: {
+      name: $("#ajaxConName").val(),
+      age: $("#ajaxConAge").val(),
+    },
+    success: function (data) {
+      alert("ajax GET 통신 성공");
+      var $div = $("<div></div>");
+      var text = document.createTextNode(data);
+      $div.append(data);
+      $div.appendTo($("#myDiv"));
+    },
+    error: function (request, status, error) {
+      alert(
+        "code:" +
+          request.status +
+          "\n" +
+          "message:" +
+          request.responseText +
+          "\n" +
+          "error:" +
+          error
+      ); //에러 상태에 대한 세부사항 출력
+      alert(e);
+    },
+  });
+}
+
+function AjaxConPost() {
+  var url = "http://localhost:8081/Ajax-jsp/ajaxCon";
+  $.ajax({
+    type: "POST",
+    url: url,
+    dataType: "html",
+    data: {
+      name: $("#ajaxConName").val(),
+      age: $("#ajaxConAge").val(),
+    },
+    success: function (data) {
+      alert("ajax POST 통신 성공");
+      var $div = $("<div></div>");
+      var text = document.createTextNode(data);
+      $div.append(data);
+      $div.appendTo($("#myDiv"));
+    },
+    error: function (request, status, error) {
+      alert(
+        "code:" +
+          request.status +
+          "\n" +
+          "message:" +
+          request.responseText +
+          "\n" +
+          "error:" +
+          error
+      ); //에러 상태에 대한 세부사항 출력
+      alert(e);
+    },
+  });
+}
---
+++ Ajax-jsp/src/Serv.java
@@ -0,0 +1,56 @@
+//package myServlet;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Servlet implementation class Serv
+ */
+//@WebServlet("/ajaxCon")
+public class Serv extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Serv() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("application/x-json; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");    
+        
+        System.out.println("doget :"+name);
+        System.out.println("deget :"+age);
+        
+        out.print("Get 통신 : 안녕 내 이름은 "+name+"이고 나이는 "+age+"란다"); //response    
+    }
+ 
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("application/x-json; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");    
+        
+        System.out.println("doget :"+name);
+        System.out.println("deget :"+age);
+            
+        out.println("Post통신 : 안녕 내 이름은 "+name+"이고 나이는 "+age+"란다"); //response
+    }
+}
